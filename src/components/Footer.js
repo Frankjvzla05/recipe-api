703d831cb44b413ef1159b354640b4d184b8779a
frank <fmaduro@ecomsur.com>
2021-12-08 13:21:06 -0300

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => (
+    <footer>
+
+        <p>&copy; { new Date().getFullYear() } - Frank Cortez</p>
+    </footer>
+
+);
+
+export default Footer;
